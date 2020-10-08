654fd7c790db319c6557c68edeaaec8f8749f6a0
Sade Babatunde <sade_babatunde@t-online.de>
2020-10-08 13:44:47 +0200

    done

--- ExtraAufgabe/ExtraAufgabe.ts
+++ ExtraAufgabe/ExtraAufgabe.ts
@@ -1,1 +1,14 @@
-window.promp("Gebe deinen Namen ein ")
+window.addEventListener("load", my Function(): void{
+
+    function myFunction(): void {
+        var txt: string;
+        var person: string = prompt("Gebe deinen Namen ein", "Sade Babatunde")
+        if (person == null || person =="") {
+            txt = "User cancelled the prompt."; 
+    
+        } else {
+            txt = "Hallo" + person + "| Einen wunderschönen Tag wüsche ich";
+        }
+        document.getElementById("demo").innerHTML = txt;
+    }
+}
