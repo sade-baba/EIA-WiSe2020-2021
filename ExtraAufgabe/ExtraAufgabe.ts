e4ee12cd736b69259eec03fdaf95a9b56f3d3a0d
Sade Babatunde <sade_babatunde@t-online.de>
2020-10-10 18:18:42 +0200

    .

--- ExtraAufgabe/ExtraAufgabe.ts
+++ ExtraAufgabe/ExtraAufgabe.ts
@@ -1,11 +1,11 @@
 //abgeleitet von W3schools
 
-window.addEventListener("load", function(): void {
+window.addEventListener("load", function() {
 
-    let person string = prompt("Wie heißt du?", "");
+    let person: string = prompt("Wie heißt du?", "");
 
         if (person != null ) {
-       document.getElementById("name").innerHTML = "Huhuuu" + person + ",ich wünsche dir einen wunderschönen Tag";
+       document.getElementById("name").innerHTML = "Huhuuu" + person + ", ich wünsche dir einen wunderschönen Tag";
          }
  });
 
