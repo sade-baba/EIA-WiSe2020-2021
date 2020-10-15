7eae3399f2f51cf6e22246d2d86672cc44c40cdc
Sade Babatunde <sade_babatunde@t-online.de>
2020-10-15 11:52:07 +0200

    .

--- ExtraAufgabe/ExtraAufgabe.js
+++ ExtraAufgabe/ExtraAufgabe.js
@@ -1,1 +1,9 @@
-window.promp()
+"use strict";
+//abgeleitet von W3schools
+window.addEventListener("load", function () {
+    let person = prompt("Wie heißt du?", "");
+    if (person != null) {
+        document.getElementById("name").innerHTML = "Huhuuu" + person + ", ich wünsche dir einen wunderschönen Tag";
+    }
+});
+//# sourceMappingURL=ExtraAufgabe.js.map
